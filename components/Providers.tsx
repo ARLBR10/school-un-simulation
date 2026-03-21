edfcb37bd9c9bb6ac828f1dfd5b5ba979b40fad5
ARLBR10 <71902040+ARLBR10@users.noreply.github.com>
2026-03-20 22:55:38 -0300

    feat: Added Better Auth UI
    https://better-auth-ui.com/

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -4,6 +4,9 @@ import { ReactNode } from "react";
 import { ConvexReactClient } from "convex/react";
 import { authClient } from "@/lib/auth-client";
 import { ConvexBetterAuthProvider } from "@convex-dev/better-auth/react";
+import { AuthUIProvider } from "@daveyplate/better-auth-ui";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
@@ -25,6 +28,28 @@ export function ConvexClientProvider({
   );
 }
 
+export function AuthProvider({ children }: { children: ReactNode }) {
+  const router = useRouter();
+  return (
+    <AuthUIProvider
+      authClient={authClient}
+      navigate={router.push}
+      replace={router.replace}
+      onSessionChange={() => {
+        // Clear router cache (protected routes)
+        router.refresh();
+      }}
+      Link={Link}
+    >
+      {children}
+    </AuthUIProvider>
+  );
+}
+
 export default function AllProviders({ children }: { children: ReactNode }) {
-  return <ConvexClientProvider>{children}</ConvexClientProvider>;
+  return (
+    <AuthProvider>
+      <ConvexClientProvider>{children}</ConvexClientProvider>
+    </AuthProvider>
+  );
 }
